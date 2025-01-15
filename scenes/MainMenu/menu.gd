da067eaaf02ac80441245469ef8bdf3c8769168c
Maurice Kleindienst <support@mousewerk.de>
2025-01-16 00:42:44 +0100

    changed menu items

--- scenes/MainMenu/menu.gd
+++ scenes/MainMenu/menu.gd
@@ -22,7 +22,7 @@ func _on_multiplayer_button_pressed() -> void:
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Settings/Settings.tscn")
-	print ("Settings Pressed") 
+	print ("settings Pressed") 
 
 
 func _on_exit_button_pressed() -> void:
