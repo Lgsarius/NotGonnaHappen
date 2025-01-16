a0661af78d59342d2e75109c3826df5570c360c1
Ophoois <alexander.fm.merz@web.de>
2025-01-16 14:34:34 +0100

    POMMES

--- player.gd
+++ player.gd
@@ -24,7 +24,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		%Old_man.play_idle_animation()
 	
-	const DAMAGE_RATE = 500.0
+	const DAMAGE_RATE = 100
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
