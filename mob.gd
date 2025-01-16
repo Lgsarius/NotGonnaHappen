34cd867f47001a93f6055503ae9e0734adbd5a75
Ophoois <alexander.fm.merz@web.de>
2025-01-16 17:31:01 +0100

    new enemy pathfinding

--- mob.gd
+++ mob.gd
@@ -5,6 +5,7 @@ var health = 3
 var speed = 150
 @onready var player = get_node("/root/Game/Player")
 @onready var animation = $AnimatedSprite2D
+@onready var nav_agent = $NavigationAgent2D as NavigationAgent2D
 
 func _ready():
 	animation.play("idle")
@@ -12,9 +13,16 @@ func _ready():
 	
 
 func _physics_process(delta: float) -> void:
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * speed
+	#var direction = global_position.direction_to(player.global_position)
+	#velocity = direction * speed
+	#move_and_slide()
+	var next_path_pos := nav_agent.get_next_path_position()
+	var dir := global_position.direction_to(next_path_pos)
+	velocity = dir * speed
 	move_and_slide()
+	
+func makepath():
+	nav_agent.target_position = player.global_position
 	
 
 func take_damage():
@@ -36,3 +44,7 @@ func drop_loot():
 	var new_orb = preload("res://orb.tscn").instantiate()
 	new_orb.global_position = global_position
 	get_parent().call_deferred("add_child",new_orb)
+
+
+func _on_timer_timeout() -> void:
+	makepath()
