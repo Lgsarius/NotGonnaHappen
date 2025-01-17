1bf6d57aca37165f80b567c89ef82b856b0cc7d3
Ophoois <alexander.fm.merz@web.de>
2025-01-17 23:29:25 +0100

    Upgrade Implementation

--- Utility/item_option.gd
+++ Utility/item_option.gd
@@ -1,16 +1,29 @@
 extends Panel
 
-@onready var game = get_tree().get_first_node_in_group("Game")
 
-signal selected_upgrade()
+@onready var lblName = $lbl_name
+@onready var lblDescription = $lbl_description
+@onready var lblLevel = $lbl_level
+@onready var itemIcon = $ColorRect/ItemIcon
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+
+
+var item = null
+signal selected_upgrade(upgrade)
 
 func _ready():
-	connect("selected_upgrade",Callable(game,"upgrade_character"))
-	
+	connect("selected_upgrade",Callable(player,"upgrade_character"))
+	if item == null:
+		item = "food"
+	lblName.text = UpgradeDb.UPGRADES[item]["displayname"]
+	lblDescription.text  =UpgradeDb.UPGRADES[item]["details"]
+	lblLevel.text = UpgradeDb.UPGRADES[item]["level"]
+	#itemIcon.texture = load(UpgradeDb.UPGRADES[item]["icon"])
 
 
 
 
 
 func _on_button_pressed() -> void:
-	selected_upgrade.emit()
+	selected_upgrade.emit(item)
--- player.gd
+++ player.gd
@@ -2,11 +2,24 @@ extends CharacterBody2D
 signal loot_collected
 signal health_depleted
 signal level_up
+
+
+@onready var levelPanel = $LevelUp
+@onready var upgradeOptions = %UpgradeOptions
+@onready var itemOptions =preload("res://Utility/item_option.tscn")
+
 var health = 100.0
+var maxhealth =100.0
+var movement_speed = 300
+var armor = 0
+var spell_cooldown = 0
+var spell_size = 0
+var additional_attacks = 0
 var experience = 0
 var experience_level = 1
 var collected_experience = 0
-var speed = 300
+var collected_upgrades  = []
+var upgrade_options = []
 const DAMAGE_RATE = 100
 
 
@@ -42,7 +55,7 @@ func _physics_process(delta: float) -> void:
 	if dashing:
 		velocity = direction * dash_speed
 	else:
-		velocity = direction * speed
+		velocity = direction * movement_speed
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
@@ -120,9 +133,49 @@ func set_expbar(set_value = 1, set_max_value=100):
 func levelup():
 	level_up.emit()
 	score.text = str(experience_level)
-	var gun = preload("res://gun.tscn").instantiate()
-	call_deferred("add_child", gun)
+	print("LevelUP")
+	levelPanel.visible = true
+	levelPanel.process_mode = Node.PROCESS_MODE_ALWAYS
+	var options = 0
+	var optionsmax = 3
+	var option_choice = itemOptions.instantiate()
+	option_choice.item = get_random_item()	
+	upgradeOptions.add_child(option_choice)
+	option_choice.get_child(-1).grab_focus()
+	options +=1
+	while options < optionsmax:
+		option_choice = itemOptions.instantiate()
+		option_choice.item = get_random_item()	
+		upgradeOptions.add_child(option_choice)
+		options += 1
+	get_tree().paused = true
 
+func upgrade_character(upgrade):
+	match upgrade:
+		"armor1","armor2","armor3","armor4":
+			armor += 1
+		"speed1","speed2","speed3","speed4":
+			movement_speed += 20.0
+		"tome1","tome2","tome3","tome4":
+			spell_size += 0.10
+		"scroll1","scroll2","scroll3","scroll4":
+			spell_cooldown += 0.05
+		"ring1","ring2":
+			additional_attacks += 1
+		"food":
+			health += 20
+			health = clamp(health,0,maxhealth)
+	
+	
+	
+	var option_children = upgradeOptions.get_children()
+	for child in upgradeOptions.get_children():
+		child.queue_free()
+	upgrade_options.clear()
+	collected_upgrades.append(upgrade)
+	levelPanel.visible = false
+	get_tree().paused = false
+	
 
 func _on_dash_timer_timeout() -> void:
 	dashing = false
@@ -132,4 +185,29 @@ func _on_dash_cooldown_timeout() -> void:
 	can_dash = true
 	$dash_cooldown.stop()
 	
+
+func get_random_item():
+	var dblist = []
+	for i in UpgradeDb.UPGRADES:
+		if i in collected_upgrades:
+			pass
+		elif i in upgrade_options:
+			pass
+		elif UpgradeDb.UPGRADES[i]["type"] == "item":
+			pass
+		elif UpgradeDb.UPGRADES[i]["prerequisite"].size() > 0:
+			var to_add = true
+			for n in UpgradeDb.UPGRADES[i]["prerequisite"]:
+				if not n in collected_upgrades:
+					to_add  =false
+			if to_add:
+				dblist.append(i)
+		else:
+			dblist.append(i)
+	if dblist.size() > 0:
+		var randomitem = dblist.pick_random()
+		upgrade_options.append(randomitem)
+		return randomitem
+	else:
+		return null
 	
